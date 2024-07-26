8649a6fbcf11fdbf712d78d19979f4925fe4055b
chgoutham199 <chgoutham199@gmail.com>
2024-07-26 10:37:29 +0530

    Initial commit  (setup)

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app= express();
+const port=6969;
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+});
